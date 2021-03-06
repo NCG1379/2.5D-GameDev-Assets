67f56c0cf2e79b68095146ed7edc8ae9e691dce0
NCG1379 <30814732+NCG1379@users.noreply.github.com>
2021-03-05 20:44:36 -0500

    File Upload

---
+++ Assets/Scripts/Player_Script/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.velocity = new Vector2(1.0f, 0.0f);
+        }else if (Input.GetKey(KeyCode.A))
+        {
+            rb.velocity = new Vector2(-1.0f, 0.0f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0.0f, 0.0f);
+        }
+
+
+
+    }
+}
